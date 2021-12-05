3cc0a07fcea42020450fed9bc0f6568efec250f5
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-05 17:38:33 -0600

    refactor: refactor stock symbol test validator

--- spec/lib/stock_validator_spec.rb
+++ spec/lib/stock_validator_spec.rb
@@ -7,8 +7,15 @@ require 'rack/test'
 RSpec.describe StockValidator do
   include Rack::Test::Methods
 
-  it 'validates that a symbol is correct' do
-    result = described_class.call(:AAPL)
-    expect(result).to be_truthy
-  end
+    describe ".call" do
+        context 'for a valid stock symbol' do
+
+            it 'should return true when the symbol exists' do
+                result = described_class.call(:AAPL)
+                expect(result).to be_truthy
+            end
+
+        end
+    end
+
 end
