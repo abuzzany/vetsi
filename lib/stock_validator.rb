940aff34b8234ef182e589394600680ef02b4f50
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-05 17:42:45 -0600

    test: add test for validating unexisted stock symbol

--- lib/stock_validator.rb
+++ lib/stock_validator.rb
@@ -1,6 +1,7 @@
 # This class validates a stock symbol in NASDAQ API.
 class StockValidator
     def self.call(stock_symbol)
+        return '' unless stock_symbol
         return true if stock_symbol == :AAPL
         false
     end
--- spec/lib/stock_validator_spec.rb
+++ spec/lib/stock_validator_spec.rb
@@ -10,9 +10,14 @@ RSpec.describe StockValidator do
     describe ".call" do
         context 'for a valid stock symbol' do
 
-            it 'should return true when the symbol exists' do
+            it 'should returns true when the symbol exists' do
                 result = described_class.call(:AAPL)
                 expect(result).to be_truthy
+            end
+
+            it 'should returns false when the symbol exists' do
+                result = described_class.call(:FAEKAAPL)
+                expect(result).to be_falsy
             end
 
         end
