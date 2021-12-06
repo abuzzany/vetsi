b79a8f942978bd412fe1558bd8439623cffe84cf
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-05 18:04:52 -0600

    style: exec rubocop stock symbol validator

--- lib/stock_validator.rb
+++ lib/stock_validator.rb
@@ -1,8 +1,9 @@
 # This class validates a stock symbol in NASDAQ API.
 class StockValidator
-    def self.call(stock_symbol)
-        return '' unless stock_symbol
-        return true if stock_symbol == :AAPL
-        false
-    end
+  def self.call(stock_symbol)
+    return '' unless stock_symbol
+    return true if stock_symbol == :AAPL
+
+    false
+  end
 end
