0e3c444aaa7c6397f7564366247a4720af589342
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-09 16:02:03 -0600

    feat: creat transactions migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_024444) do
+ActiveRecord::Schema.define(version: 2021_12_09_023216) do
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "transaction_type", null: false
+    t.integer "share_quantity", null: false
+    t.integer "share_price", null: false
+    t.datetime "created_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
