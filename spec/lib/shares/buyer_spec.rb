0850ff7799020d84a6fcbeb4d6b27ca27a37ecde
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-12 19:13:48 -0600

    refactor: remove ineccesary reference

--- spec/lib/shares/buyer_spec.rb
+++ spec/lib/shares/buyer_spec.rb
@@ -1,6 +1,4 @@
 RSpec.describe Shares::Buyer do
-  include Rack::Test::Methods
-
   describe '.call' do
     context 'for an invalid stock symbol' do
       it 'should returns a not found error when the symbol doe not exist' do
