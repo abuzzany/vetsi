5ea85ccca426072a154cce0df8995b88769faaa8
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-14 17:04:34 -0600

    feat: add stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_023216) do
+ActiveRecord::Schema.define(version: 2021_12_14_225809) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "stock_symbol", null: false
+    t.float "total_shares", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.integer "user_id", null: false
