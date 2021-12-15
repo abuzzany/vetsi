4da76b02acad684ca0491c18e5e537cc3031661e
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-14 19:21:39 -0600

    feat: add stock_symbol to transaction model

--- db/migrate/20211209023216_create_transactions.rb
+++ db/migrate/20211209023216_create_transactions.rb
@@ -3,6 +3,7 @@ class CreateTransactions < ActiveRecord::Migration[6.1]
     create_table :transactions do |t|
       t.integer :user_id, null: false
       t.integer :transaction_type, null: false
+      t.string  :stock_symbol, null: false
       t.integer :share_quantity, null: false
       t.integer :share_price, null: false
       t.timestamps(null: false)
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_12_14_225809) do
   create_table "transactions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "transaction_type", null: false
+    t.string "stock_symbol", null: false
     t.integer "share_quantity", null: false
     t.integer "share_price", null: false
     t.datetime "created_at", precision: 6, null: false
