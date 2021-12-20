c03523a90ab774f7328fc79009fa3f6200c920dd
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-19 23:53:30 -0600

    refactor: remove stock model

--- models/stock.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# This models represents the current share stock of a user.
-class Stock < ActiveRecord::Base
-  validates_presence_of :user_id,
-                        :stock_symbol,
-                        :total_shares
-end
