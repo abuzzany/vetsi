db7cdc43dbafb70b98e72091aa825c8962e07b9c
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-19 21:02:44 -0600

    feat: create class for calculate quantity of hold shares

---
+++ lib/stocks/calculate_stock_quantity.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Stocks
+  # This class calculates the quantity for given
+  # stock symbol.
+  class CalculateStockQuantity
+    attr_accessor :user_id,
+                  :stock_symbol,
+                  :transaction_type
+
+    def self.run(user_id, stock_symbol, transaction_type)
+      new(user_id, stock_symbol, transaction_type).run
+    end
+
+    def initialize(user_id, stock_symbol, transaction_type)
+      @user_id = user_id
+      @stock_symbol = stock_symbol
+      @transaction_type = transaction_type
+    end
+
+    def run
+      Transaction.select(:share_quantity).where(
+        user_id: user_id,
+        transaction_type: transaction_type,
+        stock_symbol: stock_symbol
+      ).sum(:share_quantity)
+    end
+  end
+end
