620c35e6f52fe57d28cf0c6a2350e938e865af38
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 13:56:20 -0600

    feat: remove frozen string literal

--- spec/lib/nasdaq_client/quotes_spec.rb
+++ spec/lib/nasdaq_client/quotes_spec.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
+require './spec/fixtures/stubs/nasdaq_api_stub'
 
 RSpec.describe NasdaqClient::Quotes do
+  include NasdaqApiStubs
+
   describe '.new' do
     context 'for a valid stock symbol' do
+      before(:each) do
+        allow(HTTParty).to receive(:get).and_return(valid_stock_symbol_response)
+      end
+
       it 'creates an instance of NasdaqClient::Quotes' do
         expect(described_class.new(:AAPL)).to be_an_instance_of(NasdaqClient::Quotes)
       end
@@ -11,7 +18,7 @@ RSpec.describe NasdaqClient::Quotes do
         result = described_class.new(:AAPL)
         expect(result.symbol).to be_eql('AAPL')
         expect(result.company_name).to be_eql('Apple Inc. Common Stock')
-        expect(result.last_sale_price).to be_eql('$175.915')
+        expect(result.last_sale_price).to be_eql(150.0)
       end
     end
 
