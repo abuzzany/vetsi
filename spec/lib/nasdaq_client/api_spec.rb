d5279866a41c390f01280d1f2eabe0f09989ade6
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 13:14:54 -0600

    test: add test for invalid stock symbol

--- spec/lib/nasdaq_client/api_spec.rb
+++ spec/lib/nasdaq_client/api_spec.rb
@@ -4,15 +4,21 @@ RSpec.describe NasdaqClient::Api do
   describe '.new' do
     context 'for a valid stock symbol' do
       it 'creates an instance of NasdaqClient::Api' do
-        result = described_class.new
-        expect(result).to be_an_instance_of(NasdaqClient::Api)
+        expect(described_class.new).to be_an_instance_of(NasdaqClient::Api)
       end
 
-      it 'returns the stock info for a given symbol' do
+      it 'returns the stock information for a given symbol' do
         response = described_class.new.get('quote/AAPL/info')
         expect(response['status']['rCode']).to be_eql(200)
         expect(response['data']['symbol']).to be_eql('AAPL')
         expect(response['data']['companyName']).to be_eql('Apple Inc. Common Stock')
+      end
+    end
+
+    context 'for a invalid stock symbol' do
+      it 'returns error code 400' do
+        response = described_class.new.get('quote/FAKEAAPL/info')
+        expect(response['status']['rCode']).to be_eql(400)
       end
     end
   end
