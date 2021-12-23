0c2a6f18a46207ce3ebf6664b092d988b0150cd6
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 13:48:51 -0600

    test: implement stub

--- spec/lib/nasdaq_client/api_spec.rb
+++ spec/lib/nasdaq_client/api_spec.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
+require './spec/fixtures/stubs/nasdaq_api_stub'
+
 RSpec.describe NasdaqClient::Api do
+  include NasdaqApiStubs
+
   describe '.new' do
     context 'for a valid stock symbol' do
+      before(:each) do
+        allow(HTTParty).to receive(:get).and_return(valid_stock_symbol_response)
+      end
+
       it 'creates an instance of NasdaqClient::Api' do
         expect(described_class.new).to be_an_instance_of(NasdaqClient::Api)
       end
 
       it 'returns the stock information for a given symbol' do
         response = described_class.new.get('quote/AAPL/info')
+
         expect(response['status']['rCode']).to be_eql(200)
         expect(response['data']['symbol']).to be_eql('AAPL')
         expect(response['data']['companyName']).to be_eql('Apple Inc. Common Stock')
+        expect(response['data']['primaryData']['lastSalePrice']).to be_eql('$150.00')
       end
     end
 
