b30c068b85395f02dc74a2f6b7f6f4f91dcbf00f
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-22 20:25:23 -0600

    test: add test for wallet endopoint

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'The HelloWorld App' do
     Sinatra::Application
   end
 
-  describe '#users#stocks#buy' do
+  describe '#users#wallet' do
     context 'for an existing user' do
       it 'returns the detail of the stock bought' do
         user = User.create(email: 'abuzzany@gmail.com')
@@ -35,4 +35,29 @@ RSpec.describe 'The HelloWorld App' do
       end
     end
   end
+
+  describe '#users#stocks#buy' do
+    context 'for a valid stock symbol' do
+      it 'returns the detail of the stock share bought' do
+        user = User.create(email: 'abuzzany@gmail.com')
+
+        params = {
+            stock_symbol: 'AAPL',
+            share_quantity: 10
+        }
+  
+        post "api/v1/users/#{user.id}/stocks/buy", params.to_json
+        
+        response = JSON.parse(last_response.body)
+        
+        expect(last_response).to be_ok
+        expect(response['user_id']).to be_eql(user.id)
+        expect(response['transaction_type']).to be_eql('buy')
+        expect(response['stock_symbol']).to be_eql('AAPL')
+        expect(response['share_quantity']).to be_eql(10)
+        expect(response['total_amount']).to be_eql(1756.3999999999999)
+      end
+    end
+  end
+
 end
