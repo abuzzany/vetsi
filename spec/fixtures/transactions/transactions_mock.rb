1f96b9eb0d866f4a9e44c41c811d057e21fa08a8
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 17:17:53 -0600

    feat: modify total amout of mocks

--- spec/fixtures/transactions/transactions_mock.rb
+++ spec/fixtures/transactions/transactions_mock.rb
@@ -13,21 +13,21 @@ module TransactionsMock
                        transaction_type: :buy,
                        stock_symbol: :AAPL,
                        share_quantity: 4,
-                       share_price: 122,
-                       total_amount: 4 * 122)
+                       share_price: 300,
+                       total_amount: 4 * 300)
 
     Transaction.create(user_id: user_id,
                        transaction_type: :sell,
                        stock_symbol: :AAPL,
                        share_quantity: 4,
-                       share_price: 119,
-                       total_amount: 4 * 119)
+                       share_price: 120,
+                       total_amount: 4 * 120)
 
     Transaction.create(user_id: user_id,
                        transaction_type: :buy,
                        stock_symbol: :TSLA,
                        share_quantity: 5,
-                       share_price: 200,
-                       total_amount: 5 * 200)
+                       share_price: 90,
+                       total_amount: 5 * 90)
   end
 end
