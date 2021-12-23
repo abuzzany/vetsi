5d05b580dc94c7fbcc24939dd519f98a709eb386
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 15:52:29 -0600

    feat: implement stub

--- spec/lib/services/stocks/calculate_profit_loss_spec.rb
+++ spec/lib/services/stocks/calculate_profit_loss_spec.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require './spec/fixtures/transactions/transactions_mock'
+require './spec/fixtures/stubs/nasdaq_api_stub'
 
 RSpec.describe Stocks::CalculateProfitLoss do
   include TransactionsMock
+  include NasdaqApiStubs
 
   describe '.run' do
     context 'for a valid stock symbol' do
+      before(:each) do
+        allow(HTTParty).to receive(:get).and_return(valid_stock_symbol_response)
+      end
+
       it 'should calculates the profit or loss amount' do
         user = User.create(email: 'abuzzany@gmail.com')
 
@@ -14,7 +20,7 @@ RSpec.describe Stocks::CalculateProfitLoss do
 
         result = described_class.run(user.id, :AAPL)
 
-        expect(result).to be_eql(1711.3999999999999)
+        expect(result).to be_eql(1500.0)
       end
     end
   end
