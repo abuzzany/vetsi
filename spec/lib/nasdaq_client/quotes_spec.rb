788469b306069e84c1077b1fbf5d7f2d5f849637
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 13:20:19 -0600

    test: update test desctiption

--- spec/lib/nasdaq_client/quotes_spec.rb
+++ spec/lib/nasdaq_client/quotes_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe NasdaqClient::Quotes do
   describe '.new' do
     context 'for a valid stock symbol' do
       it 'creates an instance of NasdaqClient::Quotes' do
-        result = described_class.new(:AAPL)
-        expect(result).to be_an_instance_of(NasdaqClient::Quotes)
+        expect(described_class.new(:AAPL)).to be_an_instance_of(NasdaqClient::Quotes)
       end
 
-      it 'returns the stock information for a given symbol' do
+      it 'returns the stock information for a given stock symbol' do
         result = described_class.new(:AAPL)
         expect(result.symbol).to be_eql('AAPL')
         expect(result.company_name).to be_eql('Apple Inc. Common Stock')
@@ -17,7 +16,7 @@ RSpec.describe NasdaqClient::Quotes do
     end
 
     context 'for an invalid stock symbol' do
-      it 'raises a NasdaqClient::QuoteException exception' do 
+      it 'raises a NasdaqClient::QuoteException exception' do
         expect { described_class.new(:FAKEAAPL) }.to raise_error(NasdaqClient::QuoteException)
       end
     end
