7dc90619ac0d814e779333f909922920b6d265d4
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-23 17:19:39 -0600

    refactor: calculate profit loss percentage

--- lib/services/stocks/calculate_profit_loss.rb
+++ lib/services/stocks/calculate_profit_loss.rb
@@ -5,7 +5,8 @@ module Stocks
   # a stock for a given user.
   class CalculateProfitLoss
     attr_accessor :user_id,
-                  :stock_symbol
+                  :stock_symbol,
+                  :last_sale_price
 
     def self.run(user_id, stock_symbol)
       new(user_id, stock_symbol).run
@@ -14,21 +15,46 @@ module Stocks
     def initialize(user_id, stock_symbol)
       @user_id = user_id
       @stock_symbol = stock_symbol
+      @last_sale_price = NasdaqClient::Quotes.new(stock_symbol).last_sale_price
     end
 
     def run
-      total_shares = bought_shares - selled_shares
-      total_shares * NasdaqClient::Quotes.new(stock_symbol).last_sale_price
+      (((current_stock_value - bought_stock_value) / current_stock_value) * 100).round(2)
     end
 
     private
 
-    def bought_shares
-      Shares::CalculateHeldQuantity.run(user_id, stock_symbol, :buy)
+    def bought_stock_value
+      bought_shares_value - sold_shares_value
     end
 
-    def selled_shares
-      Shares::CalculateHeldQuantity.run(user_id, stock_symbol, :sell)
+    def current_stock_value
+      total_shares = bought_shares - sold_shares
+      total_shares * last_sale_price
+    end
+
+    def bought_shares
+      @bought_shares ||= Shares::CalculateHeldQuantity.run(user_id, stock_symbol, :buy)
+    end
+
+    def sold_shares
+      @sold_shares ||= Shares::CalculateHeldQuantity.run(user_id, stock_symbol, :sell)
+    end
+
+    def bought_shares_value
+      Transaction.where(
+        user_id: user_id,
+        transaction_type: :buy,
+        stock_symbol: stock_symbol
+      ).sum(:total_amount)
+    end
+
+    def sold_shares_value
+      Transaction.where(
+        user_id: user_id,
+        transaction_type: :sell,
+        stock_symbol: stock_symbol
+      ).sum(:total_amount)
     end
   end
 end
