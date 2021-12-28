0f0c7bd6f7258efac260fa601bab15ec94b7fdde
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-28 13:23:14 -0600

    feat: add vetsi to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 ENV['RACK_ENV'] = 'test'
 
-require './app'
+require './vetsi'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
