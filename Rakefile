879daa3da76f992eddd58edf0d94e84e29b8f217
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-28 20:40:38 -0600

    feat: las try

--- Rakefile
+++ Rakefile
@@ -1,10 +1,2 @@
-# frozen_string_literal: true
-
-# Rakefile
+require './vetsi'
 require 'sinatra/activerecord/rake'
-
-namespace :db do
-  task :load_config do
-    require './vetsi'
-  end
-end
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,19 @@
-require 'bundler/setup'
-Bundler.require
+require 'sinatra/activerecord'Bundler.require
 
-configure :production, :development, :test do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/calendar_development')
-
+configure :development, :test do
+  set :database, 'sqlite://development.db'
+end
+ 
+configure :production do
+  # Database connection
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
   ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
   )
 end
