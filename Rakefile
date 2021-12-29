ab37d3111d654a523c302eae25e30c27c2f74c5e
Angel Francisco Buzany Ramirez <angelfranciscobuzanyramirez@MacBook-Pro-de-ADMIN-RTD.local>
2021-12-28 21:10:10 -0600

    feat: adjust database creation

--- Rakefile
+++ Rakefile
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require './vetsi'
 require 'sinatra/activerecord/rake'
--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,32 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/activerecord'
 
-configure :development, :test do
-  set :database, 'sqlite://development.db'
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'vetsi-development'
+  )
 end
- 
+
+configure :test do
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'vetsi-test'
+  )
+end
+
 configure :production do
   # Database connection
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
- 
+
   ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
+    adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host: db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..],
+    encoding: 'utf8'
   )
 end
